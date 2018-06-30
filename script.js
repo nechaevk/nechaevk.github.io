eb0fa940e7be07a7f0110b781a1c3b6bd066f2e8
Кирилл Нечаев <33162301+nechaevk@users.noreply.github.com>
2018-06-30 22:28:56 +0300

    888P

--- script.js
+++ script.js
@@ -1,45 +1,103 @@
-const keyboard = {
-    topRow: 'qwertyuiop[]',
-    middleRow: 'asdfghjkl;\'\\',
-    bottomRow: 'zxcvbnm,./'
-}
+const keyboard = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''], ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']];
 
-const createButton = (content, ...classNames) => {
-    const button = document.createElement('button');//создаем кнопку 
-    button.classList.add(...classNames);//добавили класс
-    button.innerText = content;
-    return button;
-}
+const notes = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si'];
 
-const createRow = () => {
-    const row = document.createElement('div');
-    row.classList.add('keyboard__row');
-    return row;
-}
-
-const createKeyboard = (keyboard) => {
-    const keyboardElement = document.createElement('div');
-    keyboardElement.classList.add('keyboard');
-
-    for (key in keyboard) {
-        const keyRow = createRow();
-
-        for (let i = 0; i < keyboard[key].length; i++) {
-            const button = createButton(keyboard[key][i], 'keyboard__button');
-            keyRow.append(button);
+function createLayout() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < keyboard[i].length; j++) {
+            let button = document.createElement('button');
+            button.innerHTML = keyboard[i][j];
+            if (i == 0) {
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
+                const up = document.querySelector(".top")
+                up.appendChild(button, up.firstChild);
+            } else if (i == 1) {
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
+                const mid = document.querySelector(".mid")
+                mid.appendChild(button, mid.firstChild);
+                button.className = "middlerow button";
+            } else if (i == 2) {
+                if (j > 6) {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j % 7])
+                } else {
+                    button.className = "toprow button";
+                    button.setAttribute('data-note', notes[j])
+                }
+                const bot = document.querySelector(".bot")
+                bot.appendChild(button, bot.firstChild);
+                button.className = "bottomrow button";
+            }
         }
-
-        keyboardElement.append(keyRow);
     }
-
-    const spaceHandler = createRow();
-    const spaceButton = createButton('_____________', 'keyboard__button', 'keyboard__button_wide');
-    spaceHandler.append(spaceButton);
-    keyboardElement.append(spaceHandler);
-
-    document.body.append(keyboardElement);
 }
 
-createKeyboard(keyboard);
+const checker = document.querySelector(".checkBox");
+
+const playSound = note => {
+    const audio = document.querySelector(`audio[data-note=${note}]`);
+    audio.currentTime = 0;
+    audio.play();
+};
 
 
+const buttons = Array.from(document.querySelectorAll("button"));
+const temp = document.querySelector(".output")
+
+function callback() {
+
+    let letter = event.key;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < keyboard[i].length; j++) {
+            if (letter == keyboard[i][j]) {
+                temp.innerHTML += letter;
+                if (i == 0) {
+                    const findAllClass = document.querySelectorAll(".toprow");
+                    findAllClass[j].className = "toprow button keyboard__btn--active";
+
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
+
+
+                } else if (i == 1) {
+                    const findAllClass = document.querySelectorAll(".middlerow");
+                    findAllClass[j].className = "middlerow button keyboard__btn--active";
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
+                } else {
+                    const findAllClass = document.querySelectorAll(".bottomrow");
+                    findAllClass[j].className = "bottomrow button keyboard__btn--active";
+                    if (checker.checked == true) {
+                        let note = findAllClass[j].getAttribute('data-note');
+                        playSound(note);
+                    }
+                }
+            }
+        }
+    }
+    window.addEventListener("keyup", callOut);
+}
+
+function callOut() {
+    let findClassToRemove = document.querySelector(".keyboard__btn--active");
+    findClassToRemove.classList.remove("keyboard__btn--active");
+}
+
+window.addEventListener("keydown", callback);
+createLayout();
